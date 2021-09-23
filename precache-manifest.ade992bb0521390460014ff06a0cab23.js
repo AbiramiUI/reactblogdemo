da44a659630262e704aa8f20294d9ec3106b7d9d
AbiramiUI <abirami.ui@gmail.com>
2021-09-23 16:16:04 +0530

    Updates

---
+++ precache-manifest.ade992bb0521390460014ff06a0cab23.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dec20848e2bba3d341373360ce70afd",
+    "url": "/reactblogdemo/index.html"
+  },
+  {
+    "revision": "dc0192fd1dcc0180250a",
+    "url": "/reactblogdemo/static/css/2.10355efa.chunk.css"
+  },
+  {
+    "revision": "d4687090df56e7693cc2",
+    "url": "/reactblogdemo/static/css/main.78032e53.chunk.css"
+  },
+  {
+    "revision": "dc0192fd1dcc0180250a",
+    "url": "/reactblogdemo/static/js/2.0cb73b14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactblogdemo/static/js/2.0cb73b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4687090df56e7693cc2",
+    "url": "/reactblogdemo/static/js/main.07df8e90.chunk.js"
+  },
+  {
+    "revision": "97843139dbbc659f1e07",
+    "url": "/reactblogdemo/static/js/runtime-main.cf55417e.js"
+  },
+  {
+    "revision": "cb48b4d093d1f3be6a7f608d9a10461d",
+    "url": "/reactblogdemo/static/media/about.cb48b4d0.jpg"
+  },
+  {
+    "revision": "80961a00ad954f1df9a3356d8e386900",
+    "url": "/reactblogdemo/static/media/affection-baby-baby.80961a00.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactblogdemo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactblogdemo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactblogdemo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactblogdemo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactblogdemo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2854cda262a455525320eb55751d5f30",
+    "url": "/reactblogdemo/static/media/logo.2854cda2.png"
+  },
+  {
+    "revision": "13ad316bf606c371e9f32ad600e9f949",
+    "url": "/reactblogdemo/static/media/memories-from.13ad316b.jpg"
+  }
+]);
